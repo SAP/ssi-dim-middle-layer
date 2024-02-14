e3e642a76e9e1757500e8a1e65a610cb3f2499fc
Phil Schneider <info@philschneider.de>
2024-02-14 21:00:06 +0100

    build: initial implementation commit
    * feat(client): add dim client
    * add process worker
    * add SAP open source default setup
    * add helm chart
    * add dockerfiles
    * add workflows
    * remove eclipse code of conduct
    * create tag only once all images are built and increase job permission
    * feat(authorization): add role authorization (#19)
    * chore(release): add version-check to create and push git tag step
    * feat(statusList): add statuslist endpoints (#22)
    * chore: add release-helm-chart as required for create-tag job to make output from release-helm-chart available to create-tag job
    * chore: add dependencies file
    * chore(release): add permission to read contents for image release (#12)

---
+++ src/clients/Dim.Clients/Api/Dim/DimClient.cs
@@ -0,0 +1,215 @@
+/********************************************************************************
+ * Copyright (c) 2024 BMW Group AG
+ * Copyright 2024 SAP SE or an SAP affiliate company and ssi-dim-middle-layer contributors.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using Dim.Clients.Extensions;
+using Dim.Clients.Token;
+using Org.Eclipse.TractusX.Portal.Backend.Framework.ErrorHandling;
+using Org.Eclipse.TractusX.Portal.Backend.Framework.HttpClientExtensions;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace Dim.Clients.Api.Dim;
+
+public class DimClient : IDimClient
+{
+    private readonly IBasicAuthTokenService _basicAuthTokenService;
+    private readonly IHttpClientFactory _clientFactory;
+
+    public DimClient(IBasicAuthTokenService basicAuthTokenService, IHttpClientFactory clientFactory)
+    {
+        _basicAuthTokenService = basicAuthTokenService;
+        _clientFactory = clientFactory;
+    }
+
+    public async Task<CreateCompanyIdentityResponse> CreateCompanyIdentity(BasicAuthSettings dimBasicAuth, string hostingUrl, string baseUrl, string tenantName, bool isIssuer, CancellationToken cancellationToken)
+    {
+        var client = await _basicAuthTokenService.GetBasicAuthorizedClient<DimClient>(dimBasicAuth, cancellationToken).ConfigureAwait(false);
+        var data = new CreateCompanyIdentityRequest(new Payload(
+            hostingUrl,
+            new Bootstrap("Holder with IATP", "Holder IATP", Enumerable.Repeat("IATP", 1)),
+            isIssuer ?
+                Enumerable.Empty<Key>() :
+                new Key[]
+                {
+                    new("SIGNING"),
+                    new("SIGNING_VC"),
+                }));
+        var result = await client.PostAsJsonAsync($"{baseUrl}/api/v2.0.0/companyIdentities", data, JsonSerializerExtensions.Options, cancellationToken)
+            .CatchingIntoServiceExceptionFor("create-company-identity", HttpAsyncResponseMessageExtension.RecoverOptions.INFRASTRUCTURE,
+                async m =>
+                {
+                    var message = await m.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return (false, message);
+                }).ConfigureAwait(false);
+        try
+        {
+            var response = await result.Content
+                .ReadFromJsonAsync<CreateCompanyIdentityResponse>(JsonSerializerExtensions.Options, cancellationToken)
+                .ConfigureAwait(false);
+            if (response == null)
+            {
+                throw new ServiceException("Response was empty", true);
+            }
+
+            return response;
+        }
+        catch (JsonException je)
+        {
+            throw new ServiceException(je.Message);
+        }
+    }
+
+    public async Task<JsonDocument> GetDidDocument(string url, CancellationToken cancellationToken)
+    {
+        var client = _clientFactory.CreateClient("didDocumentDownload");
+        using var result = await client.GetStreamAsync(url, cancellationToken).ConfigureAwait(false);
+        var document = await JsonDocument.ParseAsync(result, cancellationToken: cancellationToken).ConfigureAwait(false);
+        return document;
+    }
+
+    public async Task<string> CreateApplication(BasicAuthSettings dimAuth, string dimBaseUrl, string tenantName, CancellationToken cancellationToken)
+    {
+        var client = await _basicAuthTokenService.GetBasicAuthorizedClient<DimClient>(dimAuth, cancellationToken).ConfigureAwait(false);
+        var data = new CreateApplicationRequest(new ApplicationPayload(
+            "catena-x-portal",
+            $"Catena-X Portal MIW for {tenantName}",
+            6));
+        var result = await client.PostAsJsonAsync($"{dimBaseUrl}/api/v2.0.0/applications", data, JsonSerializerExtensions.Options, cancellationToken)
+            .CatchingIntoServiceExceptionFor("create-application", HttpAsyncResponseMessageExtension.RecoverOptions.INFRASTRUCTURE,
+                async m =>
+                {
+                    var message = await m.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return (false, message);
+                }).ConfigureAwait(false);
+        try
+        {
+            var response = await result.Content
+                .ReadFromJsonAsync<CreateApplicationResponse>(JsonSerializerExtensions.Options, cancellationToken)
+                .ConfigureAwait(false);
+            if (response == null)
+            {
+                throw new ServiceException("Response was empty", true);
+            }
+
+            return response.Id;
+        }
+        catch (JsonException je)
+        {
+            throw new ServiceException(je.Message);
+        }
+    }
+
+    public async Task<string> GetApplication(BasicAuthSettings dimAuth, string dimBaseUrl, string applicationId, CancellationToken cancellationToken)
+    {
+        var client = await _basicAuthTokenService.GetBasicAuthorizedClient<DimClient>(dimAuth, cancellationToken).ConfigureAwait(false);
+        var result = await client.GetAsync($"{dimBaseUrl}/api/v2.0.0/applications/{applicationId}", cancellationToken)
+            .CatchingIntoServiceExceptionFor("get-application", HttpAsyncResponseMessageExtension.RecoverOptions.INFRASTRUCTURE,
+                async m =>
+                {
+                    var message = await m.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return (false, message);
+                }).ConfigureAwait(false);
+        try
+        {
+            var response = await result.Content
+                .ReadFromJsonAsync<ApplicationResponse>(JsonSerializerExtensions.Options, cancellationToken)
+                .ConfigureAwait(false);
+            if (response == null)
+            {
+                throw new ServiceException("Response must not be null");
+            }
+
+            return response.ApplicaitonKey;
+        }
+        catch (JsonException je)
+        {
+            throw new ServiceException(je.Message);
+        }
+    }
+
+    public async Task AssignApplicationToCompany(BasicAuthSettings dimAuth, string dimBaseUrl, string applicationKey, Guid companyId, CancellationToken cancellationToken)
+    {
+        var client = await _basicAuthTokenService.GetBasicAuthorizedClient<DimClient>(dimAuth, cancellationToken).ConfigureAwait(false);
+        var data = new CompanyIdentityPatch(new ApplicationUpdates(Enumerable.Repeat(applicationKey, 1)));
+        await client.PatchAsJsonAsync($"{dimBaseUrl}/api/v2.0.0/companyIdentities/{companyId}", data, JsonSerializerExtensions.Options, cancellationToken)
+            .CatchingIntoServiceExceptionFor("assign-application", HttpAsyncResponseMessageExtension.RecoverOptions.INFRASTRUCTURE,
+                async m =>
+                {
+                    var message = await m.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return (false, message);
+                }).ConfigureAwait(false);
+    }
+
+    public async Task<string> GetStatusList(BasicAuthSettings dimAuth, string dimBaseUrl, Guid companyId, CancellationToken cancellationToken)
+    {
+        var client = await _basicAuthTokenService.GetBasicAuthorizedClient<DimClient>(dimAuth, cancellationToken).ConfigureAwait(false);
+        var result = await client.GetAsync($"{dimBaseUrl}/api/v2.0.0/companyIdentities/{companyId}/revocationLists", cancellationToken);
+        try
+        {
+            var response = await result.Content
+                .ReadFromJsonAsync<StatusListListResponse>(JsonSerializerExtensions.Options, cancellationToken)
+                .ConfigureAwait(false);
+            if (response == null)
+            {
+                throw new ServiceException("Response must not be null");
+            }
+
+            if (!response.Data.Any(x => x.RemainingSpace > 0))
+            {
+                throw new ConflictException("There is no status list with remaining space, please create a new one.");
+            }
+
+            return response.Data.First(x => x.RemainingSpace > 0).StatusListCredential;
+        }
+        catch (JsonException je)
+        {
+            throw new ServiceException(je.Message);
+        }
+    }
+
+    public async Task<string> CreateStatusList(BasicAuthSettings dimAuth, string dimBaseUrl, Guid companyId, CancellationToken cancellationToken)
+    {
+        var client = await _basicAuthTokenService.GetBasicAuthorizedClient<DimClient>(dimAuth, cancellationToken).ConfigureAwait(false);
+        var data = new CreateStatusListRequest(new CreateStatusListPaypload(new CreateStatusList("StatusList2021", DateTimeOffset.UtcNow.ToString("yyyyMMdd"), "New revocation list", 2097152)));
+        var result = await client.PostAsJsonAsync($"{dimBaseUrl}/api/v2.0.0/companyIdentities/{companyId}/revocationLists", data, JsonSerializerExtensions.Options, cancellationToken)
+            .CatchingIntoServiceExceptionFor("assign-application", HttpAsyncResponseMessageExtension.RecoverOptions.INFRASTRUCTURE,
+                async m =>
+                {
+                    var message = await m.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return (false, message);
+                }).ConfigureAwait(false);
+        try
+        {
+            var response = await result.Content
+                .ReadFromJsonAsync<CreateStatusListResponse>(JsonSerializerExtensions.Options, cancellationToken)
+                .ConfigureAwait(false);
+            if (response == null)
+            {
+                throw new ServiceException("Response must not be null");
+            }
+
+            return response.RevocationVc.Id;
+        }
+        catch (JsonException je)
+        {
+            throw new ServiceException(je.Message);
+        }
+    }
+}
---
+++ src/web/Dim.Web/BusinessLogic/DimBusinessLogic.cs
@@ -0,0 +1,116 @@
+/********************************************************************************
+ * Copyright (c) 2024 BMW Group AG
+ * Copyright 2024 SAP SE or an SAP affiliate company and ssi-dim-middle-layer contributors.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+using Dim.Clients.Api.Cf;
+using Dim.Clients.Api.Dim;
+using Dim.Clients.Token;
+using Dim.DbAccess;
+using Dim.DbAccess.Repositories;
+using Dim.Entities.Enums;
+using Dim.Web.ErrorHandling;
+using Microsoft.Extensions.Options;
+using Org.Eclipse.TractusX.Portal.Backend.Framework.ErrorHandling;
+
+namespace Dim.Web.BusinessLogic;
+
+public class DimBusinessLogic : IDimBusinessLogic
+{
+    private readonly IDimRepositories _dimRepositories;
+    private readonly ICfClient _cfClient;
+    private readonly IDimClient _dimClient;
+    private readonly DimSettings _settings;
+
+    public DimBusinessLogic(IDimRepositories dimRepositories, ICfClient cfClient, IDimClient dimClient, IOptions<DimSettings> options)
+    {
+        _dimRepositories = dimRepositories;
+        _cfClient = cfClient;
+        _dimClient = dimClient;
+        _settings = options.Value;
+    }
+
+    public async Task StartSetupDim(string companyName, string bpn, string didDocumentLocation, bool isIssuer)
+    {
+        var processStepRepository = _dimRepositories.GetInstance<IProcessStepRepository>();
+        var processId = processStepRepository.CreateProcess(ProcessTypeId.SETUP_DIM).Id;
+        processStepRepository.CreateProcessStep(ProcessStepTypeId.CREATE_SUBACCOUNT, ProcessStepStatusId.TODO, processId);
+
+        _dimRepositories.GetInstance<ITenantRepository>().CreateTenant(companyName, bpn, didDocumentLocation, isIssuer, processId, _settings.OperatorId);
+
+        await _dimRepositories.SaveAsync().ConfigureAwait(false);
+    }
+
+    public async Task<string> GetStatusList(string bpn, CancellationToken cancellationToken)
+    {
+        var (exists, companyId, instanceId) = await _dimRepositories.GetInstance<ITenantRepository>().GetCompanyAndInstanceIdForBpn(bpn).ConfigureAwait(false);
+        if (!exists)
+        {
+            throw NotFoundException.Create(DimErrors.NO_COMPANY_FOR_BPN, new ErrorParameter[] { new("bpn", bpn) });
+        }
+
+        if (companyId is null)
+        {
+            throw ConflictException.Create(DimErrors.NO_COMPANY_ID_SET);
+        }
+
+        if (instanceId is null)
+        {
+            throw ConflictException.Create(DimErrors.NO_INSTANCE_ID_SET);
+        }
+
+        var dimDetails = await _cfClient.GetServiceBindingDetails(instanceId.Value, cancellationToken).ConfigureAwait(false);
+        var dimAuth = new BasicAuthSettings
+        {
+            TokenAddress = $"{dimDetails.Credentials.Uaa.Url}/oauth/token",
+            ClientId = dimDetails.Credentials.Uaa.ClientId,
+            ClientSecret = dimDetails.Credentials.Uaa.ClientSecret
+        };
+        var dimBaseUrl = dimDetails.Credentials.Url;
+        return await _dimClient.GetStatusList(dimAuth, dimBaseUrl, companyId.Value, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task<string> CreateStatusList(string bpn, CancellationToken cancellationToken)
+    {
+        var (exists, companyId, instanceId) = await _dimRepositories.GetInstance<ITenantRepository>().GetCompanyAndInstanceIdForBpn(bpn).ConfigureAwait(false);
+        if (!exists)
+        {
+            throw NotFoundException.Create(DimErrors.NO_COMPANY_FOR_BPN, new ErrorParameter[] { new("bpn", bpn) });
+        }
+
+        if (companyId is null)
+        {
+            throw ConflictException.Create(DimErrors.NO_COMPANY_ID_SET);
+        }
+
+        if (instanceId is null)
+        {
+            throw ConflictException.Create(DimErrors.NO_INSTANCE_ID_SET);
+        }
+
+        var dimDetails = await _cfClient.GetServiceBindingDetails(instanceId.Value, cancellationToken).ConfigureAwait(false);
+        var dimAuth = new BasicAuthSettings
+        {
+            TokenAddress = $"{dimDetails.Credentials.Uaa.Url}/oauth/token",
+            ClientId = dimDetails.Credentials.Uaa.ClientId,
+            ClientSecret = dimDetails.Credentials.Uaa.ClientSecret
+        };
+        var dimBaseUrl = dimDetails.Credentials.Url;
+        return await _dimClient.CreateStatusList(dimAuth, dimBaseUrl, companyId.Value, cancellationToken).ConfigureAwait(false);
+    }
+}
